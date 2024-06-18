08e999895e43b2c9a40aa3435ea8c87420c59acd
Elizabeth <itsalaz@icloud.com>
2024-06-18 14:18:39 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const saturdayFun = (activity= 'roller-skate') => {
+  return (`This Saturday, I want to ${activity}!`)
+
+}
+  console.log(saturdayFun())
+  console.log(saturdayFun('bungee jump'))
+
+
+  const mondayWork = (duty= 'go to the office') => {
+    return (`This Monday, I will ${duty}.`) 
+  }
+
+    console.log(mondayWork())
+    console.log(mondayWork('work from home'))
+
+    function wrapAdjective(wrapper = "*") {
+      return function (adjective= "special") {  // return function
+        return (`You are ${wrapper}${adjective}${wrapper}!`) // wrapper wraps around 
+      }
+    }
+      console.log(wrapAdjective("%")("a dedicated programmer"))
